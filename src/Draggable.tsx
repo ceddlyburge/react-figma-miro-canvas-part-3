0aacd4c3d91f165f6796f3c37837804f3f1f766e
Cedd Burge <ceddlyburge@gmail.com>
2024-04-30 10:17:50 +0100

    Make some basic optimisations

--- src/Draggable.tsx
+++ src/Draggable.tsx
@@ -1,27 +1,32 @@
 import { useDraggable } from "@dnd-kit/core";
 import { ZoomTransform } from "d3-zoom";
+import { memo, useCallback, useMemo } from "react";
 import { Card } from "./App";
 
-export const Draggable = ({
-  card,
-  canvasTransform,
-}: {
-  card: Card;
-  canvasTransform: ZoomTransform;
-}) => {
-  const { attributes, listeners, setNodeRef, transform } = useDraggable({
-    id: card.id,
-  });
+export const Draggable = memo(
+  ({
+    card,
+    canvasTransform,
+  }: {
+    card: Card;
+    canvasTransform: ZoomTransform;
+  }) => {
+    const { attributes, listeners, setNodeRef, transform } = useDraggable({
+      id: card.id,
+    });
 
-  return (
-    <div
-      className="card"
-      style={{
-        position: "absolute",
-        top: `${card.coordinates.y * canvasTransform.k}px`,
-        left: `${card.coordinates.x * canvasTransform.k}px`,
-        transformOrigin: "top left",
-        ...(transform
+    const preventDefault = useCallback(
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      (e: any) => {
+        listeners?.onPointerDown?.(e);
+        e.preventDefault();
+      },
+      [listeners]
+    );
+
+    const cssTransform = useMemo(
+      () =>
+        transform
           ? {
               // temporary change to this position when dragging
               transform: `translate3d(${transform.x}px, ${transform.y}px, 0px) scale(${canvasTransform.k})`,
@@ -29,18 +34,29 @@ export const Draggable = ({
           : {
               // zoom to canvas zoom
               transform: `scale(${canvasTransform.k})`,
-            }),
-      }}
-      ref={setNodeRef}
-      {...listeners}
-      {...attributes}
-      // this stops the event bubbling up and triggering the canvas drag
-      onPointerDown={(e) => {
-        listeners?.onPointerDown?.(e);
-        e.preventDefault();
-      }}
-    >
-      {card.text}
-    </div>
-  );
-};
+            },
+      [canvasTransform.k, transform]
+    );
+
+    return (
+      <div
+        className="card"
+        style={{
+          position: "absolute",
+          top: `${card.coordinates.y * canvasTransform.k}px`,
+          left: `${card.coordinates.x * canvasTransform.k}px`,
+          transformOrigin: "top left",
+          ...cssTransform,
+        }}
+        ref={setNodeRef}
+        {...listeners}
+        {...attributes}
+        // this stops the event bubbling up and triggering the canvas drag
+        onPointerDown={preventDefault}
+      >
+        {card.text}
+      </div>
+    );
+  }
+);
+Draggable.displayName = "Draggable";
